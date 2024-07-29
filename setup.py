5596d42a431c242f038ae0c01fdb2b465fd8d908
Trevor <trev0r@users.noreply.github.com>
2024-07-29 10:50:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ import re
 import sys
 
 import setuptools
-import setuptools.command.test
 
 NAME = 'django-celery-beat'
 PACKAGE = 'django_celery_beat'
@@ -104,18 +103,6 @@ else:
 # -*- %%% -*-
 
 
-class pytest(setuptools.command.test.test):
-    user_options = [('pytest-args=', 'a', 'Arguments to pass to pytest')]
-
-    def initialize_options(self):
-        setuptools.command.test.test.initialize_options(self)
-        self.pytest_args = []
-
-    def run_tests(self):
-        import pytest
-        sys.exit(pytest.main(self.pytest_args))
-
-
 setuptools.setup(
     name=NAME,
     packages=setuptools.find_packages(exclude=[
@@ -134,7 +121,6 @@ setuptools.setup(
     python_requires='>=3.8',
     install_requires=reqs('default.txt') + reqs('runtime.txt'),
     tests_require=reqs('test.txt') + reqs('test-django.txt'),
-    cmdclass={'test': pytest},
     classifiers=classifiers,
     entry_points={
         'celery.beat_schedulers': [
